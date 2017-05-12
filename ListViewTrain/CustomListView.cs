fe13f31cd377c9140b08ac50afb4c895f7fe4413
panachai <panachai.ny@gmail.com>
2017-05-12 18:01:23 +0700

    update layout cardview

--- ListViewTrain/CustomListView.cs
+++ ListViewTrain/CustomListView.cs
@@ -8,72 +8,71 @@ using Square.Picasso;
 
 namespace ListViewTrain
 {
-	public class CustomListView : BaseAdapter
-	{
-		Activity activity;
-		List<DataModel> userList;
+    public class CustomListView : BaseAdapter
+    {
+        Activity activity;
+        List<DataModel> userList;
 
-		public CustomListView(Activity activity, List<DataModel> userList)
-		{
-			this.activity = activity;
-			this.userList = userList;
-		}
+        public CustomListView(Activity activity, List<DataModel> userList)
+        {
+            this.activity = activity;
+            this.userList = userList;
+        }
 
-		public override int Count
-		{
-			get
-			{
-				return userList.Count;
-			}
-		}
+        public override int Count
+        {
+            get
+            {
+                return userList.Count;
+            }
+        }
 
-		public override Java.Lang.Object GetItem(int position)
-		{
-			return position;
-		}
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
 
-		public override long GetItemId(int position)
-		{
-			return position;
-		}
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
 
-		public override View GetView(int position, View convertView, ViewGroup parent)
-		{
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
 
-			View view = convertView;
-			ViewHolder viewHolder;
+            View view = convertView;
+            ViewHolder viewHolder;
 
-			if (view == null)
-			{
-				view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.listview_row, parent, false);
-				viewHolder = new ViewHolder();
+            if (view == null)
+            {       //activity.LayoutInflater.Inflate(Android.Resource.Layout.listview_row, null);
+                view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.listview_row, parent, false);
+                viewHolder = new ViewHolder();
 
-				viewHolder.imvShow = view.FindViewById<ImageView>(Resource.Id.imvShow);
-				viewHolder.txtTitle = view.FindViewById<TextView>(Resource.Id.txtTitle);
-				viewHolder.txtDes = view.FindViewById<TextView>(Resource.Id.txtDes);
+                viewHolder.imvShow = view.FindViewById<ImageView>(Resource.Id.imvShow);
+                viewHolder.txtTitle = view.FindViewById<TextView>(Resource.Id.txtTitle);
+                viewHolder.txtDes = view.FindViewById<TextView>(Resource.Id.txtDes);
 
+                view.Tag = viewHolder;
+            }
+            else
+            {
+                viewHolder = (ViewHolder)view.Tag; //don't know..
+            }
 
-				view.Tag = viewHolder;
-			}
-			else
-			{
-				viewHolder = (ViewHolder)view.Tag; //don't know..
-			}
+            viewHolder.txtTitle.Text = userList[position].Title;
+            viewHolder.txtDes.Text = userList[position].Des; //string.Format("{0}", userList[position].Des)
 
-			viewHolder.txtTitle.Text = userList[position].Title;
-			viewHolder.txtDes.Text = userList[position].Des; //string.Format("{0}", userList[position].Des)
+            Picasso.With(activity)
+                   .Load(userList[position].Image)
+                .Into(viewHolder.imvShow);
+            return view;
+        }
 
-			Picasso.With(activity)
-				   .Load(userList[position].Image)
-				.Into(viewHolder.imvShow);
-			return view;
-		}
-
-		private class ViewHolder : Java.Lang.Object
-		{
-			public ImageView imvShow;
-			public TextView txtTitle;
-			public TextView txtDes;
-		}
-	}
+        private class ViewHolder : Java.Lang.Object
+        {
+            public ImageView imvShow;
+            public TextView txtTitle;
+            public TextView txtDes;
+        }
+    }
 }
